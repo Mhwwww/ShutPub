363fa5081eb17ea1f8b69998b9f94e527e29cff3
Mhwwww <minghewang102b@gmail.com>
2023-07-25 11:18:38 +0200

    consumer meta data

--- src/main/java/org/example/Broker.java
+++ src/main/java/org/example/Broker.java
@@ -35,7 +35,7 @@ public class Broker {
 
             Map<ActiveMQDestination, Destination> destMap = broker.getBroker().getDestinationMap();
 
-            System.out.println("2222222222222222222222"+broker.getBroker().getDestinationMap());
+            System.out.println("Destination Map for the Broker <ActiveMQDestination, Destination> "+broker.getBroker().getDestinationMap());
 
 
             for (Map.Entry<ActiveMQDestination, Destination> entry : destMap.entrySet()) {
@@ -55,79 +55,6 @@ public class Broker {
             }
 
 
-
-            /*
-            * modification on 21.07
-            * */
-            //get the current transport connectors
-//            TransportConnector transportConnector =broker.getTransportConnectors().get(0);
-//            List<TransportConnection> connections = transportConnector.getConnections();
-//
-//            Thread.sleep(5000);
-//
-//            AdvisoryBroker advisoryBroker = new AdvisoryBroker(broker.getBroker());
-//            //System.out.println(advisoryBroker.getDestinationMap());
-//
-//
-//
-//            Map<ActiveMQDestination, Destination> destMap = advisoryBroker.getDestinationMap();
-//            Connection[] consumerIds = advisoryBroker.getClients();
-//
-//            for (int i = 0; i< consumerIds.length; i++){
-//                System.out.println(consumerIds[i]);
-//            }
-//
-//            int producerNum = 0;
-//            int consumerNum = 0;
-//
-//            for (Map.Entry<ActiveMQDestination, Destination> entry : destMap.entrySet()) {
-//                ActiveMQDestination key = entry.getKey();
-//                Destination value = entry.getValue();
-//
-//
-//                if(key.isTopic() && key.getPhysicalName().contains("ActiveMQ.Advisory.Producer")){
-//                    //System.out.println(key);
-//                    producerNum++;
-//
-//                    //System.out.println("there is one Producer, and the current producer number is: "+ producerNum);
-//                }
-//
-//
-//                //consumer's subscription != 0
-//                String[] result = value.toString().split(", subscriptions=");
-//                int currNum = Integer.parseInt(result[1]);
-//
-//                //it is a client for consumer, but could have multiple subscription
-//                if(currNum!=0){
-//                    //System.out.println(currNum);//the number of subscription
-//                    System.out.println("Key: " + key + ", Value: " + value);
-//
-//                    //Destination for Consumer
-//                    String destinationConsumer = key.getPhysicalName();
-//
-//                    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
-//
-//                    javax.jms.Connection connection = connectionFactory.createConnection();
-//
-//                    // Create a session
-//                    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//
-//                    ActiveMQTopic advisoryTopic = AdvisorySupport.getConsumerAdvisoryTopic(key);
-//                    AdvisoryConsumer advisoryConsumer = (AdvisoryConsumer) session.createConsumer(advisoryTopic);
-//
-//
-//                    System.out.println("Destination for Consumer Clients: "+destinationConsumer);
-//                    //TODO: get the metadata of Consumer--> property(filter info)
-//                    // using mbeans or AdvisoryConsumer
-//                    consumerNum= consumerNum + currNum;//the number of consumer
-//                }
-//                //System.out.println("Key: " + key + ", Value: " + value);
-//                }
-//
-//            System.out.println("client length: " +advisoryBroker.getClients().length);
-//
-//            System.out.println("producer number is: "+ producerNum);
-//            System.out.println("consumer number is: " + consumerNum);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
